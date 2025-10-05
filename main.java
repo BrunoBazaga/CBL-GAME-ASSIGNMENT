a88861132ee12ab9c626cea204474d666cddeea3
bruno <johndoe@email.com>
2025-10-05 21:38:40 +0200

    added setTrue()

--- main.java
+++ main.java
@@ -1,4 +1,8 @@
 public class main {
+
+    public boolean setTrue(){
+        return true;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Goodbye world");
